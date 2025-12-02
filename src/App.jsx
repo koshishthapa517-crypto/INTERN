d7b4782ced37df9acdc2092c1a697509b546afd5
koshishthapa517-crypto <koshishthapa517@gmail.com>
2025-12-02 16:48:39 +0545

    react1

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+function App() {
+var a = 1;
+  return (
+  <>
+    <div>
+      {
+        a
+      }
+    </div>
+    <p>this is a paragraph</p>
+    <input type="number" />
+    <h1>this is h1</h1>
+
+  </>
+  )
+}
+
+export default App
