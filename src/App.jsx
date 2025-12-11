f93197f0209be90c4d4c942bdedf408aedbe3463
koshishthapa517-crypto <koshishthapa517@gmail.com>
2025-12-11 15:26:30 +0545

    feat: integrate Tailwind CSS and set up routing
    - Added Tailwind CSS as a dependency and configured it in vite.config.js
    - Implemented routing using react-router with Home and Settings pages
    - Updated App component to include navigation and routes
    - Created placeholder components for Home and Settings pages
    - Imported Tailwind CSS in index.css for styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
-import TodoList from './components/TodoList.jsx';
+import { NavLink, Route, Routes } from 'react-router';
+import Home from './pages/Home.jsx';
+import Settings from './pages/Settings.jsx';
 
 const App = () => {
-  return (
-    <TodoList />
+  return (<>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/settings' element={<Settings />} />
+    </Routes>
+    <nav className='fixed top left-11 right-11 flex justify-between bg-gray-200 p-4 mt-70 rounded-full'>
+      <NavLink to="/" >Home</NavLink>
+      <NavLink to="/settings" >Settings</NavLink>
+    </nav>
+   </>
   )
 }
 
