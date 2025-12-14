9174cbb1d66911c50dc153eb4ca273166f8984d4
koshishthapa517-crypto <koshishthapa517@gmail.com>
2025-12-14 13:12:17 +0545

    feat: update title to 'INTERN' and refactor App component with custom hooks for random dog generation and counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,26 @@
-
-import { use, useEffect, useState } from 'react';
-import axios from 'axios';
+import { useRandomDog } from './hooks/useRandomDog';
+import useCounter from './hooks/useCounter';
 
 const App = () => {
-     const [randomDog,setRandomDog] = useState({});
-      const handleGenerateNewImage = async () => {
-        try {
-          const response = await axios.get('https://dog.ceo/api/breeds/image/random');
-          setRandomDog(response.data);
-        } catch (error) {
-          console.error(error);
-        }
-      };
+  const [count, increment] = useCounter(0);
+  const { randomDog, handleGenerateNewImage } = useRandomDog(increment);
 
-        useEffect(() => {
-        handleGenerateNewImage();
-      }, []);
   return (
-      <>
-    <div className="flex w-screen flex-row items-center gap-10"></div>
+    <>
+      <div className="flex w-screen flex-row items-center gap-10"></div>
       <div className="text-3xl font-bold ">Random Dog Generator</div>
-      <button className='border p-3 cursor-pointer '
-      onClick={handleGenerateNewImage}> 
-      Generate  New Image
+      <button
+        className="border p-3 cursor-pointer"
+        onClick={handleGenerateNewImage}
+      >
+        Generate New Image
       </button>
-      <img src={randomDog.message} alt=""/>
-      
+      <div className="text-xl mt-4">Images Generated: {count}</div>
+      {randomDog && randomDog.message && (
+        <img src={randomDog.message} alt="Random Dog" />
+      )}
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ src/hooks/useRandomDog.jsx
@@ -0,0 +1,23 @@
+import { useEffect } from 'react';
+import axios from 'axios';
+import { useState } from 'react';
+
+export function useRandomDog(increment) {
+  const [randomDog, setRandomDog] = useState(null);
+
+  const handleGenerateNewImage = async () => {
+    try {
+      const response = await axios.get('https://dog.ceo/api/breeds/image/random');
+      setRandomDog(response.data);
+      if (increment) increment();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    handleGenerateNewImage();
+  }, []);
+  
+  return { randomDog, handleGenerateNewImage };
+}
